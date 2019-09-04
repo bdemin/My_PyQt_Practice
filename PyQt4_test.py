7f1f8de657a64e34f8fe6143498115b58fc81fe1
bdemin <42113340+bdemin@users.noreply.github.com>
2019-09-04 19:20:12 +0200

    Added a menu bar

--- PyQt4_test.py
+++ PyQt4_test.py
@@ -6,8 +6,20 @@ class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 500, 300)
-        self.setWindowTitle("PyQt!")
+        self.setWindowTitle('PyQt!')
         self.setWindowIcon(QtGui.QIcon('logo.png'))
+
+        extractAction = QtWidgets.QAction('&GET TO THE CHOPPAH!', self)
+        extractAction.setShortcut('Ctrl+Q')
+        extractAction.setStatusTip('Leave The App')
+        extractAction.triggered.connect(self.close_application)
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(extractAction)
+
         self.home()
 
     def home(self):
@@ -16,7 +28,7 @@ class Window(QtWidgets.QMainWindow):
         btn.clicked.connect(self.close_application)
         
         btn.resize(btn.minimumSizeHint())
-        btn.move(0, 0)
+        btn.move(100, 100)
 
         self.show()
 
