bb382622e6dd285d9b0758bcd8d88938aa89dc6d
bdemin <42113340+bdemin@users.noreply.github.com>
2019-09-04 15:00:33 +0200

    Program is now OOP-based with added logo

--- PyQt4_test.py
+++ PyQt4_test.py
@@ -1,10 +1,16 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
+
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle("PyQt!")
+        self.setWindowIcon(QtGui.QIcon('logo.png'))
+        self.show()
+
 
 app = QtWidgets.QApplication(sys.argv)
-
-window = QtWidgets.QWidget()
-window.setGeometry(50, 50, 500, 300)
-window.setWindowTitle('Blah')
-
-window.show()
+GUI = Window()
+sys.exit(app.exec_())
