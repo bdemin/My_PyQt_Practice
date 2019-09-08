00672d7aaa33df0edc5fa8bb1444c12399ef8547
bdemin <42113340+bdemin@users.noreply.github.com>
2019-09-08 18:33:49 +0300

    Add a temporarily-useless editor

--- PyQt4_test.py
+++ PyQt4_test.py
@@ -11,15 +11,28 @@ class Window(QtWidgets.QMainWindow):
         self.setWindowIcon(QtGui.QIcon('logo.png'))
 
         extractAction = QtWidgets.QAction('&GET TO THE CHOPPAH!', self)
-        extractAction.setShortcut('Ctrl+Q')
+        extractAction.setShortcut('Ctrl + Q')
         extractAction.setStatusTip('Leave The App')
         extractAction.triggered.connect(self.close_application)
 
+        openEditor = QtWidgets.QAction('&Editor', self)
+        openEditor.setShortcut('Ctrl + E')
+        openEditor.setStatusTip('Open Editor')
+        openEditor.triggered.connect(self.editor)
+
+        openFile = QtWidgets.QAction('&Open File', self)
+        openFile.setShortcut('Ctrl + O')
+        openFile.setStatusTip('Open Editor')
+        openFile.triggered.connect(self.editor)
+        
         self.statusBar()
 
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('&File')
         fileMenu.addAction(extractAction)
+        
+        editorMenu = mainMenu.addMenu('&Editor')
+        editorMenu.addAction(openEditor)
 
         self.home()
 
@@ -76,6 +89,11 @@ class Window(QtWidgets.QMainWindow):
 
         self.show()
     
+
+    def editor(self):
+        self.textEdit = QtWidgets.QTextEdit()
+        self.setCentralWidget(self.textEdit)
+
 
     def color_picker(self):
         color = QtWidgets.QColorDialog.getColor()
